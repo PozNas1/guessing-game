72b2ba0e5ed5a6593b05018b354e012d30559a31
Anastasia Poznyak <anastasia.pazniak@gmail.com>
2017-09-24 19:29:59 +0300

    Gassing game is done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.curva = Math.floor((this.max + 1 + this.min) / 2);
+        return this.curva;
     }
 
     lower() {
-
+        this.max = this.curva;
     }
 
     greater() {
-
+        this.min = this.curva;
     }
 }
 
